9e4bad99520c1c962237c5bab53b5c08b5ef325e
Victoria Khatian <khatian.v@gmail.com>
2021-12-06 18:48:47 +0000

    task 4 completed

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 // export function funTime(){
 //     return y
 // };
+// to instal chalk I need:
+// 1. npm install chalk in terminal
+// 2. import chalk from 'chalk';
 
+import chalk from 'chalk';
 import myCollection from "./collections.js";
 
 function describeItem(item) {
   if (item.count === 1) {
     console.log(
-      `I have a ${item.name}. Here's what I like about it: ${item.whatILike}.`
+      `I have a ${chalk.cyan(item.name)}. Here's what I like about it: ${chalk.green(item.whatILike)}.`
     );
   } else {
     console.log(
-      `I have ${item.count} ${item.name}s. Here's what I like about them: ${item.whatILike}.`
+      `I have ${chalk.yellow(item.count)} ${chalk.cyan(item.name)}s. Here's what I like about them: ${chalk.green(item.whatILike)}.`
     );
   }
 }
