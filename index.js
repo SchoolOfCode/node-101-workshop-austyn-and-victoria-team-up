c24c5c98a31e706efb47085842f7138f3aa95ed0
Victoria Khatian <khatian.v@gmail.com>
2021-12-06 18:30:03 +0000

    task 3 completed

--- index.js
+++ index.js
@@ -1,26 +1,8 @@
 // export function funTime(){
 //     return y
 // };
-let myCollection = [
-  {
-    name: "School of Code mug",
-    count: 1,
-    whatILike: "It has my cute pixel character on it!",
-  },
-  {
-    name: "School of Code hat",
-    count: 2,
-    whatILike: "An often overlooked fashion accessory",
-  },
-  {
-    name: "School of Code pillow",
-    count: 1,
-    whatILike: "Eat. Sleep. Code. Repeat :)",
-  },
-];
-console.log(myCollection);
 
-// Create a function called describeItem, which should take in an item as a parameter (the argument handed to this function would be an item from our collection). The function should console.log a message according to how many of the item you have.
+import myCollection from "./collections.js";
 
 function describeItem(item) {
   if (item.count === 1) {
@@ -29,15 +11,13 @@ function describeItem(item) {
     );
   } else {
     console.log(
-      `I have ${item.count} ${item.name}. Here's what I like about them: ${item.whatILike}.`
+      `I have ${item.count} ${item.name}s. Here's what I like about them: ${item.whatILike}.`
     );
   }
 }
 
-describeItem(myCollection[1]);
-
 function describeCollection(array) {
-  array.forEach(describeItem, myCollection);
+  array.forEach(describeItem);
 }
 
 describeCollection(myCollection);
